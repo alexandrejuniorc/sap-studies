f11d5edbbe5c718ca12579b498c400ea32fc0c4c
Alexandre Junior <alejunior.dev@gmail.com>
2026-02-10 16:27:56 -0300

    feat: create project

---
+++ ui5/moovi/freestyle/sap-fiori-data-navigation-sample/webapp/controller/View1.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("sapfioridatanavigationsample.controller.View1", {
+        onInit: function () {
+
+        }
+    });
+});
