0640cb9d31d5f7c539ef76d25396921bb0ed414d
Alexandre Junior <alejunior.dev@gmail.com>
2026-02-28 12:31:54 -0300

    feat: create project

---
+++ ui5/freestyle_app_notion/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["studies/freestyleappnotion/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ ui5/freestyle_app_notion/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"studies/freestyleappnotion/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
