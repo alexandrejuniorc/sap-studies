35c24975259a19724da673554f3cf913c5e19779
Alexandre Junior <alejunior.dev@gmail.com>
2026-02-28 13:01:15 -0300

    feat: enable item press handling in product list

--- ui5/freestyle_app_notion/webapp/controller/Home.controller.js
+++ ui5/freestyle_app_notion/webapp/controller/Home.controller.js
@@ -1,10 +1,17 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("studies.freestyleappnotion.controller.Home", {
-        onInit() {
-        }
+      onInit() {},
+
+      onItemPress(oEvent) {
+        const oItem = oEvent.getParameter("listItem");
+        MessageToast.show(
+          `O item clicado '${oItem.getTitle()}' possui ${oItem.getCounter()} itens`,
+        );
+      },
     });
-});
+  },
+);
