8d22f5fb91943e4a2a45262c3b0876a5916660f8
Alexandre Junior <alejunior.dev@gmail.com>
2026-03-04 10:30:33 -0300

    feat: update meuServico to log messages based on request data and improve event handling

--- cap/prime-hero-academy/projeto1/srv/meuServico.js
+++ cap/prime-hero-academy/projeto1/srv/meuServico.js
@@ -1,7 +1,12 @@
-const exportSRV = function (srv) {
+const constante = "constante";
+
+module.exports = function (srv) {
   srv.on("evento", (req) => {
-    return `Hello ${req.data.msg}`;
+    if (req.data.msg === "teste") {
+      console.log("Esta mensagem foi apenas um teste!");
+    } else {
+      console.log("Esta mensagem não é um teste!");
+    }
+    return "Hello " + req.data.msg;
   });
 };
-
-module.exports = exportSRV;
